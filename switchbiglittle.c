3e21c170ae1b5e38905e4d5291a77d2628d84968
Daniel Schmidt <scahamidit@gmail.com>
2014-09-22 17:32:07 -0500

    Better Comments for switchbitlittle.c
    Added comments to switchbiglittle.c to make understanding what different parts of the code are attempting to accomplish easier to understand.

--- switchbiglittle.c
+++ switchbiglittle.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sched.h>
+#define FILE_NAME "switch01.dat"
 
 
 struct timespec tstart={0,0}, tend={0,0};
 
 int main (int argc, char* argv[])
 {
-//TODO better error checking that argv contains a valid big core and a valid small core
+//TODO better error checking that argv contains a valid core numbers
 	if( argc != 3){
 		printf("%s requires 2 arguments\n",argv[0]);
 		return 0;
@@ -17,47 +18,62 @@ int main (int argc, char* argv[])
 	int core1 = atoi(argv[1]);
 	int core2 = atoi(argv[2]);
 	
+	//file to record timing data
+	FILE *record = fopen(FILENAME,"w");
+	
+//set inital thread association
 	cpu_set_t *cpusetp;
-	cpusetp = CPU_ALLOC(4);
+	cpusetp = CPU_ALLOC(8);
+	if (cpusetp == NULL) {
+		perror("CPU_ALLOC");
+		exit(EXIT_FAILURE);
+	}
 	CPU_ZERO(cpusetp);
-	CPU_SET(2, cpusetp);
+	CPU_SET(core1, cpusetp);
+
 	if( -1 == sched_setaffinity( 0, sizeof(cpu_set_t), cpusetp) ){
-		printf("error\n");
+		printf("error 1\n");
 		return 1;
 	}
+		
+
+
 	int i;
+	int j = 0;
 //START TIMING
-	clock_gettime(CLOCK_MONOTONIC, &tstart);
-	for( i=0; i< 1000000; i++ )
-	{
-		if((i%2)==0){
-			CPU_ZERO(cpusetp);
-			CPU_SET(3, cpusetp);
-			CPU_SET(1, cpusetp);
-			if( -1 == sched_setaffinity(0, sizeof(cpu_set_t), cpusetp)){
-				printf("error\n");
-				return 2;
-			}
-		}else{
-			CPU_ZERO(cpusetp);
-			CPU_SET(2, cpusetp);
-			CPU_SET(4, cpusetp);
-			if( -1 == sched_setaffinity(0, sizeof(cpu_set_t), cpusetp)){
-				printf("error\n");
-				return 3;
+	while( j < 10000){
+		clock_gettime(CLOCK_MONOTONIC, &tstart);
+		for( i=0; i< 100; i++ )
+		{
+			//CHANGE THREAD ASSOCIATION
+			if((i%2)==0){
+				CPU_ZERO(cpusetp);
+				CPU_SET(core2, cpusetp);
+				if( -1 == sched_setaffinity(0, sizeof(cpu_set_t), cpusetp)){
+					printf("error 2\n");
+					return 2;
+				}
+			}else{
+				CPU_ZERO(cpusetp);
+				CPU_SET(core1, cpusetp);
+				if( -1 == sched_setaffinity(0, sizeof(cpu_set_t), cpusetp)){
+					printf("error 3\n");
+					return 3;
+				}
 			}
 		}
-	}
 	
-	clock_gettime(CLOCK_MONOTONIC, &tend);
+		clock_gettime(CLOCK_MONOTONIC, &tend);
 //END TIMING
 
-	printf("total time %9f\n",((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
-           ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+//RECORD DATA
+		fprintf(record,"%9f\n",((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
+		   ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+		printf("total time %9f\n",((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
+		   ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+		j++;
+	}
+	fclose(record);
 	return 0;
 }
-/*TIMING TOOL
-	for( i=0; i<4; i++){
-		if(CPU_ISSET( i, cpusetp)) printf("%i set\n",i);
-	}
-*/
+
